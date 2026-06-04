33e307fc91fd07f82e8f0c2b92c578fc32863ae5
SarvanMaurya123 <sarvanmaurya037@gmail.com>
2026-06-04 13:02:27 +0530

    login middlewere

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const accessToken = request.cookies.get("accessToken")?.value;
-
-  console.log(
-    "PATH:",
-    request.nextUrl.pathname,
-    "TOKEN:",
-    !!accessToken
-  );
-
   const isAuthPage =
     request.nextUrl.pathname === "/login" ||
     request.nextUrl.pathname === "/signup";
@@ -18,21 +9,13 @@ export function middleware(request: NextRequest) {
   const isDashboard =
     request.nextUrl.pathname.startsWith("/dashboard");
 
-  if (!accessToken && isDashboard) {
-    return NextResponse.redirect(
-      new URL("/login", request.url)
-    );
-  }
-
-  if (accessToken && isAuthPage) {
-    return NextResponse.redirect(
-      new URL("/dashboard", request.url)
-    );
+  // ⚠️ DO NOT RELY FULLY ON COOKIE HERE
+  if (isAuthPage && request.cookies.has("accessToken")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
 }
-
 export const config = {
   matcher: ["/dashboard/:path*", "/login", "/signup"],
 };
